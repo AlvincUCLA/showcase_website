0ca68e0690ce60120a05d5c176586314526bfb89
alvinc <alvincUCLA@gmail.com>
2024-02-05 21:55:37 -0800

    Add description on the new columns.

--- main.py
+++ main.py
@@ -9,5 +9,30 @@ with col1:
 
 with col2:
     st.title("Alvin Chen")
-    content = """Hi! This is Alvin."""
+    content = """
+    As a Customer Support Engineer at actnano, a company that provides life-saving 
+    nanocoating technology for electronics, I apply my technical support skills to 
+    help grow and maintain customer satisfaction. 
+    I have a Google Project Manager Career Certificate, 
+    which demonstrates my ability to plan, execute, and deliver complex projects 
+    using agile methodologies and tools. 
+    
+    With over seven years of experience in the manufacturing and research industries, 
+    I have successfully managed and supported multiple product lines, 
+    such as Flexible Lighting Solutions, AC Modules, Robotics IR Module, and LiDAR. 
+    I have led and collaborated with cross-functional teams, including hardware, 
+    firmware, software, cost, marketing, and sales, to meet customer expectations 
+    and win OEM and ODM projects. 
+    
+    I have also optimized the product quality and efficiency by improving the 
+    material selection, design, and fabrication processes. 
+    Additionally, I have a strong bilingual ability in Chinese and English, 
+    which enables me to communicate effectively with global customers and partners.
+    """
     st.info(content)
+
+content2 = """
+Below you can find my portfolio and the apps I have built in Python.
+Please feel free to reach out to me if any questions.
+"""
+st.write(content2)

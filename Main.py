391d8d7b1180c9bb9c107996319b932fc5c73ca8
alvinc <alvincUCLA@gmail.com>
2024-02-06 18:55:40 -0800

    Add navigation menu and contact us page.

--- Main.py
+++ Main.py
@@ -37,13 +37,19 @@ Please feel free to reach out to me if any questions.
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col2, col4 = st.columns([1.5, 0.3, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")

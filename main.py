43b01e7d465a4c57990e669c4dcdc51bbf8fe2d5
alvinc <alvincUCLA@gmail.com>
2024-02-06 17:31:03 -0800

    Render the app titles to 2 columns.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -17,11 +18,10 @@ with col2:
     which demonstrates my ability to plan, execute, and deliver complex projects 
     using agile methodologies and tools. 
     
-    With over seven years of experience in the manufacturing and research industries, 
+    With over 8 years of experience in the manufacturing and research industries, 
     I have successfully managed and supported multiple product lines, 
     such as Flexible Lighting Solutions, AC Modules, Robotics IR Module, and LiDAR. 
-    I have led and collaborated with cross-functional teams, including hardware, 
-    firmware, software, cost, marketing, and sales, to meet customer expectations 
+    I have led and collaborated with cross-functional teams to meet customer expectations 
     and win OEM and ODM projects. 
     
     I have also optimized the product quality and efficiency by improving the 
@@ -36,3 +36,14 @@ Below you can find my portfolio and the apps I have built in Python.
 Please feel free to reach out to me if any questions.
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
